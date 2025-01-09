0f8ff7a5f7f5d1d2f91970a38c19f479b42b7532
Bruno M. Silva <brunosilva010601@gmail.com>
2025-01-09 11:47:22 -0300

    add github pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    plugins: [vue()],
+    base: "/salient/",
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+});
